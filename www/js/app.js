4fd13fbf7b242459691e64a08db15d3fb4021978
jadman02 <jadman02@hotmail.com>
2017-08-18 16:24:48 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -6599,7 +6599,7 @@ $( ".onlineblock" ).show();
        //  ' <a href="#tab1" class="prevs button disabled" style="border-radius:5px;position:absolute;left:-37px;top:50%;margin-top:-28px;height:56px;width:56px;border:0;z-index:99;color:#2196f3;background-color:rgba(247, 247, 247, 0.952941);"><i class="pe-7s-angle-left pe-4x" style="margin-left:7px;margin-top:-1px;z-index:-1"></i></a>'+
        //                ' <a href="#tab3" class="nexts button" style="border-radius:5px;position:absolute;right:-37px;width:56px;top:50%;margin-top:-26px;height:56px;color:#2196f3;border:0;z-index:99;background-color:rgba(247, 247, 247, 0.952941);"><i class="pe-7s-angle-right pe-4x" style="margin-left:-35px;margin-top:-1px;"></i></a>'+
 
-     '<div style="position:absolute;width:100%;bottom:20px;height:400px;background-color:white;z-index:10000;"></div>'+
+     '<div style="position:absolute;width:100%;bottom:20px;height:20px;background-color:white;z-index:10000;"></div>'+
 
 '<div class="swiper-container swiper-questions" style="overflow-y:scroll;height:100%;">'+
 
