797fee4475eadfe297e0dae63bffc8759588108b
jadman02 <jadman02@hotmail.com>
2017-08-18 13:37:03 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7017,7 +7017,7 @@ swiperQuestions = myApp.swiper('.swiper-questions', {
 
 	//questionsslide  
 	  
-  var wrapperheightshould = $(".prof_" + swiper.activeIndex).height() - 100;
+  var wrapperheightshould = $(".prof_" + swiper.activeIndex).height() - 300;
 //	  alert(wrapperheightshould);
 //$( ".wrapper-questions").css("height",(wrapperheightshould - 200)+ "px");
 
