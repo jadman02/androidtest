819de65ca798c9581f7425c8cd9046e02e9166f3
jadman02 <jadman02@hotmail.com>
2017-08-19 11:20:43 +1000

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1483,8 +1483,8 @@ var loginmethod = window.localStorage.getItem("loginmethod");
 	if ($('.topdiv').length > 0) {}
 	else{
 	$('.content-here-1').html(
-   '<div style="width:100%;text-align:center;margin-top:0px;height:44px;background-color:#ccc;positin:absolute;top:0px;">'+
-          '<div class="preloader" style="margin: 0 auto;margin-top:5px;"></div>'+
+   '<div style="width:100%;text-align:center;margin-top:0px;height:44px;background-color:#ccc;position:absolute;top:0px;">'+
+          '<div class="preloader" style="margin: 0 auto;margin-top:10px;"></div>'+
 				'</div>'+
 		'<div class="no-results-div" style="background-color:white;z-index:30000000;text-align:center;margin:0 auto;width:300px;position:absolute;top:54px;left:50%;margin-left:-150px;">'+
 '<div class="topdiv">'+
